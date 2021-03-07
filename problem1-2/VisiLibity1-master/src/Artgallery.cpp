ac931db5a58ded459ba7fe40062191d40f25225e
aakashnaik150798@gmail.com <aakashnaik150798@gmail.com>
2021-03-07 12:05:15 +0530

    new

--- problem1-2/VisiLibity1-master/src/Artgallery.cpp
+++ problem1-2/VisiLibity1-master/src/Artgallery.cpp
@@ -13,16 +13,23 @@
 #include <cassert>
 #include"clipper.hpp"
 #include<math.h>
-
+#include "polypartition.h"
 using namespace std;
 using namespace ClipperLib;
-
 struct vis
 {
-	Path path;
-	IntPoint pt;
-	double area;
+	Paths path;
+	TPPLPoint pt;
+	//double area;
 
+};
+
+struct v_comp
+{
+	
+	bool flag;
+	double x,y;
+	//set<int>d;
 };
 
 int main(int argc, char* argv[])
@@ -44,24 +51,10 @@ int main(int argc, char* argv[])
 	//Seed the rand() fnc w/Unix time
 	//(only necessary once at the beginning of the program)
 	std::srand(std::time(NULL)); rand();
-
-
-	//Set geometric robustness constant
-	//:WARNING: 
-	//may need to modify epsilon for Environments with greatly varying
-	//scale of features
-	double epsilon = 0.01;
-	//std::cout << green << "The robustness constant epsilon is set to "
-		//<< epsilon << normal << std::endl;
-
-
-	/*----------Load Geometry from Files----------*/
-	//Load geometric environment model from file
+    double epsilon = 0.00001;
 	std::cout << "Loading environment file ";
 	std::string environment_file(argv[1]);
-	//Print environment filename to screen
 	std::cout << environment_file << " . . . ";
-	//Construct Environment object from file
 	VisiLibity::Environment my_environment(environment_file);
 	std::cout << "OK" << std::endl;
 
@@ -70,7 +63,164 @@ int main(int argc, char* argv[])
 	ofstream myfile, myfile1;
 	myfile.open("data.txt");
 	myfile1.open("map.txt");
-	VisiLibity::Visibility_Graph my_vis_graph((my_environment),
+	
+	
+	
+	TPPLPolyList polylist{},outlist{};
+	TPPLPoly  poly;
+
+	
+	poly.Init(my_environment[0].n());
+	poly.SetHole(false);
+	
+	for(int i=0;i<my_environment[0].n();i++)
+	{   
+		(poly.GetPoints())[i].x=my_environment[0][i].x();
+		(poly.GetPoints())[i].y=my_environment[0][i].y();
+    }
+	polylist.push_back(poly);
+
+	for(int j=1;j<=my_environment.h();j++)
+	{
+		TPPLPoly  poly;
+		poly.Init(my_environment[j].n());
+		poly.SetHole(true);
+		for(int i=0;i<my_environment[j].n();i++)
+		{   
+			(poly.GetPoints())[i].x=my_environment[j][i].x();
+			(poly.GetPoints())[i].y=my_environment[j][i].y();
+    	}
+		polylist.push_back(poly);
+	}
+	
+	TPPLPartition z;
+	int y=     z.ConvexPartition_HM(&polylist,&outlist);
+    v_comp components[outlist.size()];
+	int i=0;
+	vector<set<VisiLibity::Point>>vert_poly;
+	vector<bool>flag;
+	for (auto it = (outlist).begin(); it != (outlist).end(); ++it)
+	{ 
+	    components[i].flag=true;
+		double a=0.0,b=0.0;
+		set<VisiLibity::Point>temp;
+		for(int j=0;j<it->GetNumPoints();j++)
+		{
+			VisiLibity::Point re(it->GetPoints()[j].x,it->GetPoints()[j].y);
+			temp.insert(re);
+			a+=(it->GetPoints())[j].x;
+			b+= (it->GetPoints())[j].y;
+		}
+		vert_poly.push_back(temp);
+		flag.push_back(false);
+		a=a/it->GetNumPoints();
+		b=b/it->GetNumPoints();
+		components[i].x=a;
+		components[i].y=b;
+		i++;	
+	}
+	
+	map<VisiLibity::Point,set<int>>d;
+    for(int i=0;i<my_environment.n();i++)
+	{
+		VisiLibity::Polygon polygon_temp = VisiLibity::Visibility_Polygon(my_environment(i), my_environment, epsilon);
+		for(int j=0;j<outlist.size();j++)
+		{
+			VisiLibity::Point p(components[j].x,components[j].y);
+			if(p.in(polygon_temp, epsilon))
+			{
+				if(d.find(my_environment(i))==d.end())
+				{
+					set<int>f;
+					f.insert(j);
+					
+					d[my_environment(i)]=f;
+				}
+				else
+				{
+					d[my_environment(i)].insert(j);
+				}	
+			}
+		}
+	}
+	while(!d.empty())
+	{
+		VisiLibity::Point dum;
+		int max1=-1;
+		for(auto t:d)
+		{
+			if((int)t.second.size()>max1)
+			{
+				max1=t.second.size();
+				dum.set_x(t.first.x());
+				dum.set_y(t.first.y());
+			}
+
+		}
+		set<int>g=d[dum];
+		for(auto t=d.begin();t!=d.end();t++)
+		{
+			for(auto k:g)
+			{
+				if(t->second.find(k)!=t->second.end())
+				    t->second.erase(k);
+			}
+		}
+		for(auto t:d)
+		{
+			if((t.second.empty()))
+			  d.erase(t.first);
+		}
+		
+	}
+	int tr=vert_poly.size();
+	while(tr>0)
+	{
+		map<VisiLibity::Point,int>count;
+		
+		for(int i=0;i<my_environment.n();i++)
+		{
+			int yt=0;
+			for(int j=0;j<vert_poly.size();j++)
+			{
+				if(!flag[j] &&vert_poly[j].find(my_environment(i))!=vert_poly[j].end())
+				   yt++;
+			}
+			count[my_environment(i)]=yt;
+		}
+
+		int max=-1;
+		VisiLibity::Point p;
+		for(auto itr:count)
+		{
+			if(itr.second>max)
+			 {
+				 p.set_x(itr.first.x());
+				 p.set_y(itr.first.y());
+				 max=itr.second;
+			 }
+		}
+		for(int j=0;j<vert_poly.size();j++)
+		{
+			if(vert_poly[j].find(p)!=vert_poly[j].end())
+			{	
+				flag[j]=true;
+				tr--;
+			}
+		}
+        cout<<p<<'\n';
+	}
+
+		//map<int,VisiLibity::Point>rt;
+	/*int j=0;
+	for(int i=0;i<my_environment.n();i++)
+	{
+		rt[j++]=my_environment[i];
+	}*/
+
+
+
+	/*VisiLibity::Visibility_Graph my_vis_graph((my_environment),
 		epsilon);
 		int n = my_environment.n();
 		for (int i = 0; i < n; i++)
@@ -78,16 +228,11 @@ int main(int argc, char* argv[])
 			for (int j = 0; j < n; j++)
 				if(my_vis_graph(i, j)&&i!=j)
 				myfile << i<<" "<<j<<'\n';
-			
-			//myfile << '\n';
 			myfile1 << my_environment(i)<<'\n';
 		}
 		myfile.close();
-		myfile1.close();
-	//VisiLibity::Point observer(0.5000000000 , 0.0000000000);
-	// find all visible polygon of all vertices
-	// find their area as well and store them in struct
-	VisiLibity::Polygon outer_boundary = my_environment[0];
+		myfile1.close();*/
+	/*VisiLibity::Polygon outer_boundary = my_environment[0];
 	vis vis_poly[my_environment.n()];
 	int k = 0;
 	for (int i = 0; i <= my_environment.h(); i++)
@@ -95,6 +240,7 @@ int main(int argc, char* argv[])
 		for (int j = 0; j < my_environment[i].n(); j++)
 		{
 			VisiLibity::Point observer = my_environment[i][j];
+			//cout<<observer<<'\n';
 			VisiLibity::Visibility_Polygon my_vis_poly(observer,
 				(my_environment),
 				epsilon);
@@ -105,28 +251,29 @@ int main(int argc, char* argv[])
 				a.Y = my_vis_poly[l].y();
 				x << a;
 			}
-			vis_poly[k].path = x;
+			vis_poly[k].path<<x;
 			vis_poly[k].area = abs(my_vis_poly.area());
 			IntPoint v(observer.x(), observer.y());
 			vis_poly[k].pt = v;
 			k++;
-
 		}
 		
 
-	}
+	}*/
 	
 	/*for (int i = 0; i < my_environment.n(); i++)
 		cout << vis_poly[i].pt << '\n' << vis_poly[i].area << " \n//////////\n";*/
-	double poly_area = my_environment[0].area();
-	//cout << "old area " << poly_area << '\n';
-
+    //cout<<"d";
+	
+/*	double poly_area = my_environment[0].area();
+	
 	for (int i = 1; i <= my_environment.h(); i++)
 		poly_area -= abs(my_environment[i].area());
-	cout << "area is " << poly_area << '\n';
+	//cout << "area is " << poly_area << '\n';
 	int y = 3;
-	while (poly_area > 0.1)
+	while (poly_area > 0.0001)
 	{
+
 		int ind=0;
 		double max_area = 0;
 		for (int i = 0; i < my_environment.n(); i++)
@@ -138,35 +285,47 @@ int main(int argc, char* argv[])
 			}
 			//cout << "i: " << i << " " << vis_poly[i].area << '\n';
 		}
-		if (max_area < 0.1)
+		if (max_area < 0.0001)
 			break;
 		poly_area -= max_area;
 		//cout <<"new index"<< ind <<" a: "<<vis_poly[ind].area<< '\n';
 		//cout << "Area " << poly_area << '\n';
-		for (int i = 0; i < outer_boundary.n(); i++)
+		for (int i = 0; i < my_environment.n(); i++)
 		{
-			if (vis_poly[i].area > 0.0001&&ind!=i)
+			if (vis_poly[i].area > 0.000001&&ind!=i)
 			{
 				//cout << i << '\n';
 				Paths q, w;
-				q << vis_poly[i].path;
-				w << vis_poly[ind].path;
+				q = vis_poly[i].path;
+				w = vis_poly[ind].path;
 				Clipper clpr;
 				clpr.AddPaths(q, ptSubject, true);
 				clpr.AddPaths(w, ptClip, true);
 				Paths solution;
 				clpr.Execute(ctDifference, solution, pftEvenOdd, pftEvenOdd);
-				vis_poly[i].path = solution[0];
+				vis_poly[i].path = solution;
+				//cout<<solution.size()<<'\n';
 				//cout << "Solution area is " << solution[0] << " " << Area(solution[0]) << '\n';
-				vis_poly[i].area = abs(Area(solution[0]));
+				vis_poly[i].area=0;
+				for(int j=0;j<solution.size();j++)
+				vis_poly[i].area += abs(Area(solution[j]));
 			}
 			
 		}
 		vis_poly[ind].area = 0;
+		for(int i=0;i<my_environment.n();i++)
+		{
+			cout<<vis_poly[i].pt<<" a "<<'\n';
+			cout<<vis_poly[i].path<<" b "<<'\n';
+			cout<<vis_poly[i].area<<" c "<<'\n';
+			
+		}
 		cout << vis_poly[ind].pt<<'\n';
+		cout << "old area " << poly_area <<" AA "<<max_area<< '\n';
+
 		y--;
 		//break;
-	}
+	}*/
 	/*Path a, b;
 	IntPoint x;
 	x.X = 0;
